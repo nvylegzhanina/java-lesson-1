1e9d52798919df0c77d9239957341828daeb1576
nvylegzhanina <n.vylegzhanina@icloud.com>
2020-12-08 22:05:32 +0300

    Add homework 2

--- src/com/company/HomeWork1.java
+++ src/com/company/HomeWork1.java
@@ -1,8 +1,8 @@
 package com.company;
 
-public class Main {
+public class HomeWork1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello world!");
     }
 }
